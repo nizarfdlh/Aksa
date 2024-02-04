162176362d8f96fcf6f7694c79144b81784f872d
nizarfdlh <suncodev@gmail.com>
2024-02-04 16:34:17 +0700

    add configuration

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const cookieParser = require('cookie-parser')
-
-require('dotenv').config();
-
-const PORT = process.env.PORT | 3030;
 const db = require('./database/db_config');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // MODELS 
 const User = require('./models/user_models');
+
+const app = express();
+require('dotenv').config();
+
+// PORT
+const PORT = process.env.PORT | 3030;
+
+// Bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // SIMPLE API 
 app.get('/', (req, res, next) => {

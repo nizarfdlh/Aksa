b238ac42fb10fc991a4a6f57a3613f660661e5e8
nizarfdlh <suncodev@gmail.com>
2024-02-04 16:52:05 +0700

    edit database configuration

--- database/db_config.js
+++ database/db_config.js
@@ -1,17 +1,31 @@
-const Sequelize = require('sequelize')
+const { Sequelize } = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        logging: false,
-        dialect: 'mysql'
-    }
-);
+const sequelize = new Sequelize({
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    logging: false
+});
+
+
+// const Sequelize = require('sequelize')
+
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         host: process.env.DB_HOST,
+//         logging: false,
+//         dialect: 'mysql'
+//     }
+// );
 
 sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully')
